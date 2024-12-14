0ac8690733aefeef848aac0d96ea3182696c00df
luja <lujachitrakar156@gmail.com>
2024-12-14 13:22:51 +0000

    fixing bugs

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  *@title A sample Raffle Contract
  *@author Luja Chitrakar
@@ -9,8 +12,8 @@ pragma solidity ^0.8.19;
  *@dev Implements Chainlink VRFv2.5
  */
 
-contract Raffle {
-    // custom error ||  name of function in the front for more readability
+contract Raffle is VRFConsumerBaseV2Plus {
+    // custom error ||  name oxf function in the front for more readability
     error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
@@ -18,12 +21,18 @@ contract Raffle {
     uint256 private immutable i_interval;
     uint256 private immutable s_lastTimeStamp;
     address payable[] private s_players;
+
     event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        s_vrfCoordinator.requestRandomWords();
     }
 
     function enterRaffle() external payable {
@@ -46,18 +55,19 @@ contract Raffle {
             revert();
         }
         // get random number
-        // requestId=s_vrfCoordinator.requestRandomWords(
-        //     VRFV2PlusClient.RandomWordsRequest({
-        //         keyHash:s_keyHash,
-        //         subId:s_subscriptionId,
-        //         requestConfirmations:requestConfirmations,
-        //         callbackGasLimit:callbackGasLimit,
-        //         numWords:numWords,
-        //         extraArgs:VRFV3PlusClient._argsToBytes(
-        //             VRFV2PlusClient.ExtraArgsV1({nativePayment:false})
-        //         )
-        //     })
-        // )
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
     // getter Function
