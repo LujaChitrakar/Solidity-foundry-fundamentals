be51d42f97df903e3a3386ad9450ae1349af353f
luja <lujachitrakar156@gmail.com>
2024-12-21 12:11:23 +0000

    adding more tests

--- foundry-smart-contract-lottery/test/unit/RaffleTest.t.sol
+++ foundry-smart-contract-lottery/test/unit/RaffleTest.t.sol
@@ -1,45 +1,69 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
-import {Test,console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
-contract RaffleTest is Test{
+contract RaffleTest is Test {
     Raffle public raffle;
     HelperConfig public helperConfig;
 
-        uint256 entranceFee;
-        uint256 interval;
-        address vrfCoordinator;
-        bytes32 gasLane;
-        uint32 callbackGasLimit;
-        uint256 subscriptionId;
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint32 callbackGasLimit;
+    uint256 subscriptionId;
 
     // Creates a mock player
-    address public PLAYER=makeAddr("player");
-    uint256 public constant STARTING_PLAYER_BALANCE=10 ether;
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
 
-    function setUp() external{
-        DeployRaffle deployer=new DeployRaffle();
-        (raffle,helperConfig)= deployer.deployContract();
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.deployContract();
 
-        HelperConfig.NetworkConfig memory config=helperConfig.getConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-        entranceFee=config.entranceFee;
-        interval=config.interval;
-        vrfCoordinator=config.vrfCoordinator;
-        gasLane=config.gasLane;
-        callbackGasLimit=config.callbackGasLimit;
-        subscriptionId= config.subscriptionId;
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        callbackGasLimit = config.callbackGasLimit;
+        subscriptionId = config.subscriptionId;
 
         console.log("VRF Coordinator:", config.vrfCoordinator);
-require(config.vrfCoordinator != address(0), "VRF Coordinator is zero address!");
-
+        require(
+            config.vrfCoordinator != address(0),
+            "VRF Coordinator is zero address!"
+        );
     }
 
-    function testRaffleInitializesInOpenState() public view{
-        assert(raffle.getRaffleState()==Raffle.RaffleState.OPEN);
+    // test if raffle is initialized as open
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    // test if raffle revert when no enough eth
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // act/assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        raffle.enterRaffle();
+    }
+
+    // test if raffle records player when sent enough eth
+    function testRaffleRecordsPlayersWhenTheyEnter() public {
+        // arrange
+        vm.prank(PLAYER);
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+        // act
+        raffle.enterRaffle{value: entranceFee}();
+        // assert
+        address playerRecorder = raffle.getPlayer(0);
+        assert(playerRecorder == PLAYER);
     }
 }
